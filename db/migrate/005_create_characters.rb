c6771068115838b1db2d628c6cd90acf9e4b0c0d
Stephen Harrington <stephenharrington04@yahoo.com>
2018-04-27 18:37:12 +0000

    automatically backed up by learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -3,6 +3,7 @@ class CreateCharacters < ActiveRecord::Migration[5.1]
     create_table :characters do |t|
       t.string :name
       t.integer :show_id
+      t.integer :actor_id
     end
   end
 end
